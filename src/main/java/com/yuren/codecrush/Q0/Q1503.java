6c97114068c2fa1c065f11d536b63a76ee24f929
jianglu <1020447548@qq.com>
2024-11-24 02:19:01 +0800

    20241122

--- src/main/java/com/yuren/codecrush/Q0/Q1503.java
+++ src/main/java/com/yuren/codecrush/Q0/Q1503.java
@@ -9,14 +9,18 @@ package com.yuren.codecrush.Q0;
  */
 public class Q1503 {
     private static String constructPalindrome(String s, int k) {
+        System.out.println("s.length = " + s.length());
         int diff = s.length() - k;
+        System.out.println("diff = " + diff);
         if (diff == 0) {
             return new StringBuilder(s).reverse().toString();
         }
         if (isPalindrome(s.substring(s.length() - diff, s.length()))) {
+            // 字符串s diff + 1 到 s.length 是回文。 在尾巴上接k个跟头k个对称
             return new StringBuilder().append(s, 0, k).reverse().toString();
         }
         if (isPalindrome(s.substring(0, diff))) {
+            // 字符串s 0 到 diff 是回文,在头上接k个跟尾k个对称
             return new StringBuilder().append(s, s.length() - k, s.length()).reverse().toString();
         }
         return null;
@@ -36,6 +40,9 @@ public class Q1503 {
     }
 
     public static void main(String[] args) {
-        System.out.println(constructPalindrome("bab", 2));
+        System.out.println(constructPalindrome("abccc", 2));
+        System.out.println(constructPalindrome("abccc", 3));
+        System.out.println(constructPalindrome("cccba", 2));
+        System.out.println(constructPalindrome("cccba", 3));
     }
 }
