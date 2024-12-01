3c6c29334cdb071fc02ad715cbe2f95f3c8b247a
jianglu <1020447548@qq.com>
2024-12-01 23:46:59 +0800

    20241201

---
+++ src/main/java/com/yuren/codecrushlearn/Q0/Q2101.java
@@ -0,0 +1,93 @@
+package com.yuren.codecrushlearn.Q0;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author Sleepy Code Tom
+ * @date 2024-12-01 23:46
+ */
+public class Q2101 {
+
+    public boolean validateIp(String ipStr) {
+        int numCount = 0;
+        int dotCount = 0;
+        int num = 0;
+        boolean isNum = false;
+
+        for (int i = 0; i < ipStr.length(); i++) {
+            char c = ipStr.charAt(i);
+            if (isBlank(c)) {
+                if (isNum) {
+                    numCount++;
+                    if (numCount > 4) {
+                        return false;
+                    }
+                    if (numCount - dotCount > 1) {
+                        return false;
+                    }
+                    if (num > 255) {
+                        return false;
+                    }
+                    isNum = false;
+                    num = 0;
+                }
+            } else if (idDot(c)) {
+                if (isNum) {
+                    numCount++;
+                    if (numCount > 4) {
+                        return false;
+                    }
+                    if (numCount - dotCount > 1) {
+                        return false;
+                    }
+                    if (num > 255) {
+                        return false;
+                    }
+                    isNum = false;
+                    num = 0;
+                }
+                dotCount++;
+                if (dotCount > 3) {
+                    return false;
+                }
+                if (dotCount > numCount) {
+                    return false;
+                }
+            } else if (isDigit(c)) {
+                if (isNum && num == 0) {
+                    return false;
+                }
+                num = num * 10 + (c - '0');
+                isNum = true;
+            } else {
+                return false;
+            }
+        }
+        if (isNum) {
+            numCount++;
+            if (num > 255) {
+                return false;
+            }
+        }
+        if (numCount != 4) {
+            return false;
+        }
+        if (dotCount != 3) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isBlank(char c) {
+        return c == ' ';
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean idDot(char c) {
+        return c == '.';
+    }
+}
