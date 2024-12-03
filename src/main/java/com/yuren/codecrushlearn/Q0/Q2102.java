070e609964e76e25e2e060158732831f89431cfb
jianglu <1020447548@qq.com>
2024-12-04 00:00:55 +0800

    20241203

---
+++ src/main/java/com/yuren/codecrushlearn/Q0/Q2102.java
@@ -0,0 +1,33 @@
+package com.yuren.codecrushlearn.Q0;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author Sleepy Code Tom
+ * @date 2024-12-03 23:59
+ */
+public class Q2102 {
+    public int atoi(String str) {
+        int i = 0;
+        while (i < str.length() && str.charAt(i) == ' ') ++i;
+        if (i == str.length()) return 0;
+
+        int flag = 1;
+        if (str.charAt(i) == '-') {
+            flag = -1;
+            i++;
+        } else if (str.charAt(i) == '+') {
+            i++;
+        }
+
+        int num = 0;
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            num *= 10;
+            num += str.charAt(i) - '0';
+            i++;
+        }
+
+        return flag * num;
+    }
+}
