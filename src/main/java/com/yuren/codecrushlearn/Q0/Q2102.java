84afb85440d217a0a339807eafd1e31e01807a79
jianglu <1020447548@qq.com>
2024-12-05 00:30:07 +0800

    20241204

--- src/main/java/com/yuren/codecrushlearn/Q0/Q2102.java
+++ src/main/java/com/yuren/codecrushlearn/Q0/Q2102.java
@@ -10,9 +10,11 @@ package com.yuren.codecrushlearn.Q0;
 public class Q2102 {
     public int atoi(String str) {
         int i = 0;
+        // 先去掉开头的空格
         while (i < str.length() && str.charAt(i) == ' ') ++i;
         if (i == str.length()) return 0;
 
+        // 解析正负号
         int flag = 1;
         if (str.charAt(i) == '-') {
             flag = -1;
@@ -21,6 +23,7 @@ public class Q2102 {
             i++;
         }
 
+        // 逐位解析
         int num = 0;
         while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
             num *= 10;
