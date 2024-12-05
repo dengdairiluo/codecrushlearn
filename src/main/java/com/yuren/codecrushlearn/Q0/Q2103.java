4236d3807e3c61adc1fc74523665342adaeb72d5
jianglu <1020447548@qq.com>
2024-12-05 23:46:03 +0800

    20241205

---
+++ src/main/java/com/yuren/codecrushlearn/Q0/Q2103.java
@@ -0,0 +1,36 @@
+package com.yuren.codecrushlearn.Q0;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author Sleepy Code Tom
+ * @date 2024-12-05 23:44
+ */
+public class Q2103 {
+    public int atoi(String s) {
+        int i = 0;
+        int n = s.length();
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+        boolean sign = false;
+        if (i < n && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
+            sign = s.charAt(i) == '-';
+            i++;
+        }
+        int ans = 0;
+        int maxV = Integer.MAX_VALUE / 10;
+        for (; i < n && Character.isDigit(s.charAt(i)); i++) {
+            int v = s.charAt(i) - '0';
+            if (ans > maxV || ans == maxV && (!sign && v > 7 || sign && v > 8)) {
+                return 0;
+            }
+            ans = ans * 10 + v;
+        }
+        if (sign) {
+            return -ans;
+        }
+        return ans;
+    }
+}
