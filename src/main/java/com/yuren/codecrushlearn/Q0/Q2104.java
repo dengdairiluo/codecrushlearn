c97b433d5b2b0f5fe5fc29b13d1c79d53f397be1
jianglu <1020447548@qq.com>
2024-12-08 23:58:36 +0800

    20241208

--- src/main/java/com/yuren/codecrushlearn/Q0/Q2104.java
+++ src/main/java/com/yuren/codecrushlearn/Q0/Q2104.java
@@ -14,16 +14,18 @@ public class Q2104 {
 
     public List<String> split(String s, char t) {
         List<String> result = new ArrayList<>();
+        // 判断分隔符起止位置
         int start = 0;
         int end = s.indexOf(t);
         while (end != -1) {
+            // 起止之间有字符串就写入
             if (start != end) {
                 result.add(s.substring(start, end));
             }
             start = end + 1;
             end = s.indexOf(t, start);
         }
-
+        // 最后一段也要写入
         if (start < s.length()) {
             result.add(s.substring(start));
         }
