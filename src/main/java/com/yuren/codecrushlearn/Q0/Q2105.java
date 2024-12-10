c45928ad6200120c40d7d14c61adbf167c9975f9
jianglu <1020447548@qq.com>
2024-12-10 23:49:29 +0800

    20241210

--- src/main/java/com/yuren/codecrushlearn/Q0/Q2105.java
+++ src/main/java/com/yuren/codecrushlearn/Q0/Q2105.java
@@ -18,16 +18,19 @@ public class Q2105 {
         int count = 1;
 
         for (int i = 1; i < str.length(); i++) {
+            // 连续相同的字符就计数
             if (str.charAt(i) == str.charAt(i - 1)) {
                 count++;
             } else {
+                //遇到不同的字符就写入压缩格式 字符 + 次数
                 compressed.append(str.charAt(i - 1)).append(count);
                 count = 1;
             }
         }
-
+        //结尾最后写一次
         compressed.append(str.charAt(str.length() - 1)).append(count);
 
+        // 越压越长返回本身
         if (compressed.length() >= str.length()) {
             return str;
         } else {
