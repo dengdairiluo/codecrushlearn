e88d2d26b078d2deeaaefaf139955c8d61e2819c
jianglu <1020447548@qq.com>
2024-12-22 23:34:31 +0800

    20241222

--- src/main/java/com/yuren/codecrushlearn/Q0/Q2111.java
+++ src/main/java/com/yuren/codecrushlearn/Q0/Q2111.java
@@ -13,10 +13,21 @@ public class Q2111 {
         int[] row = new int[n];
         int[] column = new int[m];
 
+        // 标记有行0的行和列
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 if (matrix[i][j] == 0) {
                     row[i] = 1;
+                    column[j] = 1;
+                }
+            }
+        }
+
+        // 有0的行和列置0
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (row[i] == 1 || column[j] == 1) {
+                    matrix[i][j] = 0;
                 }
             }
         }
