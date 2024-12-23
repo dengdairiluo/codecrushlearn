ed57d0a554b06ba5ca353f396ce245903ee21738
jianglu <1020447548@qq.com>
2024-12-24 01:01:09 +0800

    20241223

---
+++ src/main/java/com/yuren/codecrushlearn/Q0/Q2112.java
@@ -0,0 +1,35 @@
+package com.yuren.codecrushlearn.Q0;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author Sleepy Code Tom
+ * @date 2024-12-24 01:00
+ */
+public class Q2112 {
+    public boolean editWord(String wrongWord, String correctWord) {
+        int m = wrongWord.length(), n = correctWord.length();
+        if (Math.abs(m - n) > 1) return false;
+        int i = 0, j = 0;
+        int count = 0;
+        while (i < m && j < n) {
+            if (wrongWord.charAt(i) != correctWord.charAt(j)) {
+                count++; // 编辑次数
+                if (m == n) { // 替换
+                    i++;
+                    j++;
+                } else if (m > n) { // 删除和添加
+                    i++;
+                } else {
+                    j++;  // 删除和添加
+                }
+            } else {
+                i++;
+                j++;
+            }
+        }
+        // 跳过次数多于1, 肯定不能一次替换
+        return count <= 1;
+    }
+}
