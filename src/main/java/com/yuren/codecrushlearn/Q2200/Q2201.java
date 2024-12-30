0cbd6db9e3ed71f3932800995390f1e78a43db37
jianglu <1020447548@qq.com>
2024-12-30 23:38:13 +0800

    20241230

--- src/main/java/com/yuren/codecrushlearn/Q2200/Q2201.java
+++ src/main/java/com/yuren/codecrushlearn/Q2200/Q2201.java
@@ -14,10 +14,13 @@ public class Q2201 {
         int caps = n;
         int totalDrinks = n;
 
+        // 判断能不能喝酒
         while (bottles >= 3 || caps >= 5) {
+            // 计算新喝的酒
             int newDrinks = bottles / 3 + caps / 5;
             totalDrinks += newDrinks;
 
+            // 更新剩余的酒瓶和瓶盖
             bottles = bottles % 3 + newDrinks;
             caps = caps % 5 + newDrinks;
         }
