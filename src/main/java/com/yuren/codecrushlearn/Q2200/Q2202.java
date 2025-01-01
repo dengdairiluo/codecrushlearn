807eb6b3898e26998d5281ae8e7a60e8edad4858
jianglu <1020447548@qq.com>
2025-01-02 00:10:37 +0800

    20250101

--- src/main/java/com/yuren/codecrushlearn/Q2200/Q2202.java
+++ src/main/java/com/yuren/codecrushlearn/Q2200/Q2202.java
@@ -11,11 +11,12 @@ import java.util.Stack;
  */
 public class Q2202 {
 
-    public int removeDigits(int num, int k) {
+    private static int removeDigits(int num, int k) {
         String strNum = Integer.toString(num);
         Stack<Character> stack = new Stack<>();
 
         for (char digit : strNum.toCharArray()) {
+            // 栈有值，删除数字不到k个，栈顶元素大于当前元素，删除栈顶元素
             while (!stack.isEmpty() && k > 0 && stack.peek() > digit) {
                 stack.pop();
                 k--;
@@ -23,6 +24,7 @@ public class Q2202 {
             stack.push(digit);
         }
 
+        // 还没删够K个，从栈顶删除
         while (k > 0) {
             stack.pop();
             k--;
@@ -39,5 +41,9 @@ public class Q2202 {
         }
 
         return result.length() == 0 ? 0 : Integer.parseInt(result.toString());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDigits(1542249, 3));
     }
 }
