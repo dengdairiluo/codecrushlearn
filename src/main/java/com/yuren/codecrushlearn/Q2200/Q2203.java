e35f5d56771b27568e0b30e0f4cc78737a5202da
jianglu <1020447548@qq.com>
2025-01-04 23:47:49 +0800

    20250103

--- src/main/java/com/yuren/codecrushlearn/Q2200/Q2203.java
+++ src/main/java/com/yuren/codecrushlearn/Q2200/Q2203.java
@@ -11,17 +11,18 @@ public class Q2203 {
     int select(int[] votes, int n){
         int candidate = 0;
         int counter = 0;
-        for(int i = 0; i < votes.length; i++){
-            if (counter == 0){
-                candidate = votes[i];
+        for (int vote : votes) {
+            if (counter == 0) {
+                candidate = vote;
             }
-            if(votes[i] == candidate){
+            if (vote == candidate) {
                 counter++;
-            }else {
-                counter --;
+            } else {
+                counter--;
             }
         }
         // 已知有一位候选人的票数过半,因此不用再判读是否存在
+        // 即，只有一个候选人，投他大于不投他
         return candidate;
     }
 }
