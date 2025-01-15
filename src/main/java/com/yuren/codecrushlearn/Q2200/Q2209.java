62428642bf2c20a0dfbcc7284fbad6f5d9c1106e
jianglu <1020447548@qq.com>
2025-01-16 00:01:00 +0800

    20250115

--- src/main/java/com/yuren/codecrushlearn/Q2200/Q2209.java
+++ src/main/java/com/yuren/codecrushlearn/Q2200/Q2209.java
@@ -10,7 +10,8 @@ package com.yuren.codecrushlearn.Q2200;
 public class Q2209 {
     public boolean jump(int[] nums) {
         int n = nums.length;
-        int maxReach = 0; // 记录当前能跳到的最远位置
+        // 记录当前能跳到的最远位置
+        int maxReach = 0;
 
         for (int i = 0; i < n; i++) {
             // 如果当前下标超过了能跳到的最远位置，则返回 false
@@ -24,6 +25,7 @@ public class Q2209 {
                 return true;
             }
         }
-        return false; // 如果循环结束后，仍然不能到达最后一个位置，则返回 false
+        // 如果循环结束后，仍然不能到达最后一个位置，则返回 false
+        return false;
     }
 }
