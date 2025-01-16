8a3aa391193d52b1f976f7cd51bd00e6c83f8020
jianglu <1020447548@qq.com>
2025-01-17 01:13:22 +0800

    20250116

---
+++ src/main/java/com/yuren/codecrushlearn/Q2200/Q2210.java
@@ -0,0 +1,25 @@
+package com.yuren.codecrushlearn.Q2200;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author Sleepy Code Tom
+ * @date 2025-01-17 01:12
+ */
+public class Q2210 {
+    public void rotate90(int[][] matrix, int n) {
+        for (int i = 0; i < n / 2; i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[n - i - 1];
+            matrix[n - i - 1] = temp;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
