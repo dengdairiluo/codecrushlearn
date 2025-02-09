4fe6777c0fe4d18d760e736f38a4cccdb7ce1bb9
jianglu <1020447548@qq.com>
2025-02-10 07:16:42 +0800

    20250210

---
+++ src/main/java/com/yuren/codecrushlearn/Q2300/Q2312.java
@@ -0,0 +1,28 @@
+package com.yuren.codecrushlearn.Q2300;
+
+import com.yuren.codecrushlearn.entities.ListNode;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author Sleepy Code Tom
+ * @date 2025-02-10 07:16
+ */
+public class Q2312 {
+    public ListNode swap(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode temp = dummy;
+        while (temp.next != null && temp.next.next != null) {
+            // 需要交换位置的两个节点
+            ListNode first = temp.next;
+            ListNode second = first.next;
+            // 第一个节点指向第二个节点的后继
+            first.next = second.next;
+            temp.next = second;
+            second.next = first;
+            temp = first;
+        }
+        return dummy.next;
+    }
+}
