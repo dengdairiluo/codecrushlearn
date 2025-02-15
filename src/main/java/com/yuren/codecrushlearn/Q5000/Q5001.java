301edb19293948f37eb946a701d80b231b484931
jianglu <1020447548@qq.com>
2025-02-15 22:56:25 +0800

    20250214

---
+++ src/main/java/com/yuren/codecrushlearn/Q5000/Q5001.java
@@ -0,0 +1,24 @@
+package com.yuren.codecrushlearn.Q5000;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author Sleepy Code Tom
+ * @date 2025-02-15 22:56
+ */
+public class Q5001 {
+    public String rotate(String s, int k) {
+        char[] str = s.toCharArray();
+        int n = str.length;
+        k = k % n;
+        for (int i = 0; i < k; ++i) {
+            char tmp = str[n-1];
+            for (int j = n-1; j > 0; --j) {
+                str[j] = str[j-1];
+            }
+            str[0] = tmp;
+        }
+        return new String(str);
+    }
+}
