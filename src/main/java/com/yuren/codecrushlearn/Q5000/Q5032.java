0742ce63b18ee1b8566d1b5289cbc92127ba3679
jianglu <1020447548@qq.com>
2025-02-22 23:38:55 +0800

    20250222

---
+++ src/main/java/com/yuren/codecrushlearn/Q5000/Q5032.java
@@ -0,0 +1,30 @@
+package com.yuren.codecrushlearn.Q5000;
+
+import com.yuren.codecrushlearn.entities.ListNode;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author Sleepy Code Tom
+ * @date 2025-02-22 23:38
+ */
+public class Q5032 {
+
+    public ListNode insert(ListNode head, int value) {
+        if (head == null) {
+            return new ListNode(value, null);
+        }
+        if (value < head.val) {
+            return new ListNode(value, head);
+        }
+        ListNode p = head;
+        while (p.next != null && p.val < value && p.next.val < value) {
+            p = p.next;
+        }
+        ListNode tmp = p.next;
+        p.next = new ListNode(value, tmp);
+        return head;
+    }
+
+}
