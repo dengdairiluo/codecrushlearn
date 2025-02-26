54c185a21c69b63ee310501e89781ca0dbc4061a
jianglu <1020447548@qq.com>
2025-02-27 00:40:08 +0800

    20250226

---
+++ src/main/java/com/yuren/codecrushlearn/Q5000/Q5034.java
@@ -0,0 +1,66 @@
+package com.yuren.codecrushlearn.Q5000;
+
+import com.yuren.codecrushlearn.entities.ListNode;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author Sleepy Code Tom
+ * @date 2025-02-27 00:39
+ */
+public class Q5034 {
+    private ListNode head;
+    private int n;
+
+    public Q5034() {
+        this.head = new ListNode(-1, null);
+        this.n = 0;
+    }
+
+    public void add(int value) {
+        add(this.n, value);
+    }
+
+    public boolean add(int index, int value) {
+        if (index < 0 || index > this.n) return false;
+
+        ListNode prev = head;
+        int i = 0;
+        while (i < index) {
+            prev = prev.next;
+            i++;
+        }
+        ListNode tmp = prev.next;
+        prev.next = new ListNode(value, tmp);
+        this.n++;
+        return true;
+    }
+
+    public int remove(int index) {
+        if (index < 0 || index >= this.n) return -1;
+
+        ListNode prev = head;
+        int i = 0;
+        while (i < index) {
+            prev = prev.next;
+            i++;
+        }
+        int ret = prev.next.val;
+        prev.next = prev.next.next;
+        this.n--;
+        return ret;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= this.n) return -1;
+
+        ListNode p = head.next;
+        int i = 0;
+        while (i < index) {
+            p = p.next;
+            i++;
+        }
+        return p.val;
+    }
+}
