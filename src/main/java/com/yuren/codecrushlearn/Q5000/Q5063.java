2872bbaa087e9d85abc19e2c0554b3278013a37f
jianglu <1020447548@qq.com>
2025-03-09 23:22:38 +0800

    20250307

--- src/main/java/com/yuren/codecrushlearn/Q5000/Q5063.java
+++ src/main/java/com/yuren/codecrushlearn/Q5000/Q5063.java
@@ -14,10 +14,13 @@ public class Q5063 {
         Stack<Character> stack = new Stack<>();
         char[] sarr = s.toCharArray();
         for (int i = 0; i < sarr.length; ++i) {
+            // 左括号入栈
             if (sarr[i] == '(' || sarr[i] == '[' || sarr[i] == '{') {
                 stack.push(sarr[i]);
             } else {
+                // 右括号出栈
                 char topValue = stack.pop();
+                // 不匹配返回false
                 if ((sarr[i] == ')' && topValue != '(')
                         || (sarr[i] == ']' && topValue != '[')
                         || (sarr[i]) == '}' && topValue != '{') {
@@ -25,6 +28,7 @@ public class Q5063 {
                 }
             }
         }
+        // 是否全部匹配
         return stack.isEmpty();
     }
 }
