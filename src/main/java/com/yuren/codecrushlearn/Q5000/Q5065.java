40153a5c5fbbd1243bf082d20fc159e8d8f47681
jianglu <1020447548@qq.com>
2025-03-11 23:43:03 +0800

    20250311

--- src/main/java/com/yuren/codecrushlearn/Q5000/Q5065.java
+++ src/main/java/com/yuren/codecrushlearn/Q5000/Q5065.java
@@ -7,7 +7,9 @@ import java.util.Stack;
 /**
  * Created with Intellij IDEA.
  * Description:
- *
+ * 设想有一堆盘子，堆太高可能会倒下来。因此，在现实生活中，盘子堆到一定高度时，就会另外堆一堆盘子。请实现数据结构 SetOfStacks ，模拟这种行为。
+ * SetStacks 由多个栈组成，并且在前一个栈填满时，新建一个栈。此外，SetOfStacks.push()、SetOfStacks.pop()、SetOfStacks.peek()、
+ * SetOfStacks.isEmpty() 应该与普通栈的操作方法相同。SetOfStacks 的构造函数的参数 sizePerStack 表示每个栈的大小。
  * @author Sleepy Code Tom
  * @date 2025-03-11 00:41
  */
