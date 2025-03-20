ba7e7818d1d8af2dbb467c7c67c91ba8a2cd7d18
jianglu <1020447548@qq.com>
2025-03-20 23:30:27 +0800

    20250320

---
+++ src/main/java/com/yuren/codecrushlearn/Q5100/Q5192.java
@@ -0,0 +1,26 @@
+package com.yuren.codecrushlearn.Q5100;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author Sleepy Code Tom
+ * @date 2025-03-20 23:29
+ */
+public class Q5192 {
+
+    private int[] mem;
+
+    public int fibonacci(int n) {
+        mem = new int[n+1];
+        return fibonacci_recur(n);
+    }
+
+    private int fibonacci_recur(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+        if (mem[n] != 0) return mem[n];
+        mem[n] = (fibonacci_recur(n-1) + fibonacci_recur(n-2))%1000000007;
+        return mem[n];
+    }
+}
